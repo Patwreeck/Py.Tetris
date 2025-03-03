6e0c6615f3d39fa6fc28e6d5c7bb188a08b80768
Patwreeck <blackforest.saints@gmail.com>
2025-03-04 03:49:14 +0800

    Update: Rotation

--- Tetris/main.py
+++ Tetris/main.py
@@ -12,15 +12,33 @@ clock = pygame.time.Clock()
 
 game = Game()
 
+GAME_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(GAME_UPDATE, 200)
+
 while True: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                game.move_left()
+            if event.key == pygame.K_RIGHT:
+                game.move_right()
+            if event.key == pygame.K_DOWN:
+                game.move_down()
+            if event.key == pygame.K_UP:
+                game.rotate()
+        if event.type == GAME_UPDATE:
+            game.move_down()
+        
+
     #Drawing
     screen.fill(dark_blue)
     game.draw(screen)
-
+   
     pygame.display.update()
     clock.tick(60)
+
+
 
